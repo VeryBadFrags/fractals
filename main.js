bf36ca0bdd8f146ad4b37dd56ee1c228709ec0d8
VeryBadFrags <1664052+VeryBadFrags@users.noreply.github.com>
2022-07-24 21:08:44 -0700

    Add script mode

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ const liveUpdateCheck = document.getElementById("liveUpdate");
 const bgColor = document.getElementById("bgColorPicker");
 const lineColor = document.getElementById("lineColorPicker");
 
+const scriptArea = document.getElementById("scriptArea");
 
 function draw() {
   showElement(loading);
@@ -43,7 +44,7 @@ function redraw() {
 function clearCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = bgColor.value;
-  ctx.fillRect(0,0,canvas.width, canvas.height);
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function drawPoly(sides, depth = 0, thickness = 1, strategy) {
@@ -212,28 +213,21 @@ function initListeners() {
     draw();
   });
 
-  // document.getElementById("drawClearButton").addEventListener("click", (e) => {
-  //   clearCanvas();
-  //   draw();
-  // });
-
   let clearButton = document.getElementById("clearButton");
   clearButton.addEventListener("click", (e) => {
     clearCanvas();
   });
 
+  let playButton = document.getElementById("playButton");
+  playButton.addEventListener("click", (e) => {
+    playScript(scriptArea.value);
+    draw();
+  });
+
   let shareButton = document.getElementById("shareButton");
   shareButton.addEventListener("click", (e) => {
     const baseUrl = window.location.href.split("?")[0];
-    const params = new URLSearchParams({
-      sides: sidesRange.value,
-      depth: depthRange.value,
-      ratio: ratioRange.value,
-      inverted: invertedCheck.checked,
-      type: strategySelect.selectedIndex + 1,
-      lc: lineColor.value,
-      bc: bgColor.value,
-    });
+    const params = generateUrlParams();
     navigator.clipboard.writeText(baseUrl + "?" + params.toString()).then(
       function () {
         copyLinkToast.show();
@@ -243,6 +237,12 @@ function initListeners() {
       }
     );
   });
+
+  document
+    .getElementById("addScriptButton")
+    .addEventListener("click", (e) => {
+      scriptArea.value += generateUrlParams() + '\n';
+    });
 
   document.getElementById("exportButton").addEventListener("click", (e) => {
     let image = new Image();
@@ -322,6 +322,18 @@ function initListeners() {
   });
 }
 
+function generateUrlParams() {
+  return new URLSearchParams({
+    s: sidesRange.value,
+    d: depthRange.value,
+    r: ratioRange.value,
+    i: invertedCheck.checked ? 1 : 0,
+    t: strategySelect.selectedIndex + 1,
+    lc: lineColor.value,
+    bg: bgColor.value,
+  });
+}
+
 function hideElement(e) {
   e.style.visibility = "hidden";
 }
@@ -332,39 +344,53 @@ function showElement(e) {
 initListeners();
 
 {
-  liveUpdateCheck.checked = false;
   let paramString = window.location.href.split("?")[1];
-  let queryString = new URLSearchParams(paramString);
-
-  if (queryString.has("sides")) {
-    sidesRange.value = queryString.get("sides");
-    sidesRange.dispatchEvent(new Event("input"));
-  }
-  if (queryString.has("depth")) {
-    depthRange.value = queryString.get("depth");
-    depthRange.dispatchEvent(new Event("input"));
-  }
-  if (queryString.has("ratio")) {
-    ratioRange.value = queryString.get("ratio");
-    ratioSlider.value = queryString.get("ratio");
-    ratioRange.dispatchEvent(new Event("input"));
-  }
-  if (queryString.has("inverted")) {
-    let invertedString = queryString.get("inverted");
-    let invertedValue = invertedString === "true" || invertedString === "1";
-    invertedCheck.checked = invertedValue;
-    // invertedCheck.dispatchEvent(new Event("change"));
-  }
-  if (queryString.has("type")) {
-    strategySelect.selectedIndex = Math.max(queryString.get("type") - 1, 0);
-  }
-  if (queryString.has("lc")) {
-    lineColor.value = queryString.get("lc");
-  }
-  if (queryString.has("bc")) {
-    bgColor.value = queryString.get("bc");
-  }
+  playScript(paramString);
   liveUpdateCheck.checked = true;
 }
 
-redraw();
+function playScript(script) {
+  liveUpdateCheck.checked = false;
+  let splitScript = script.split("\n");
+
+  if(splitScript.length <= 0) {
+    return;
+  }
+
+  let queryString = new URLSearchParams(splitScript[0]);
+  if (queryString.has("bg")) {
+    bgColor.value = queryString.get("bg");
+    clearCanvas();
+  }
+
+  for(let i = 0; i < splitScript.length; i++) {
+    let queryString = new URLSearchParams(splitScript[i]);
+    if (queryString.has("s")) {
+      sidesRange.value = queryString.get("s");
+      sidesRange.dispatchEvent(new Event("input"));
+    }
+    if (queryString.has("d")) {
+      depthRange.value = queryString.get("d");
+      depthRange.dispatchEvent(new Event("input"));
+    }
+    if (queryString.has("r")) {
+      let parsedRatio = queryString.get("r")
+      ratioRange.value = parsedRatio;
+      ratioSlider.value = parsedRatio;
+      ratioRange.dispatchEvent(new Event("input"));
+    }
+    if (queryString.has("i")) {
+      let invertedString = queryString.get("i");
+      let invertedValue = invertedString === "true" || invertedString === "1";
+      invertedCheck.checked = invertedValue;
+      // invertedCheck.dispatchEvent(new Event("change"));
+    }
+    if (queryString.has("t")) {
+      strategySelect.selectedIndex = Math.max(queryString.get("t") - 1, 0);
+    }
+    if (queryString.has("lc")) {
+      lineColor.value = queryString.get("lc");
+    }
+    draw();
+  }
+}
